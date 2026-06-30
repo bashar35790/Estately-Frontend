74502e3b7b6fd2ddb01d9059309966cc1255f3d9
bashar35790 <bashar35790@gamil.com>
2026-06-30 18:08:32 +0600

    fixed the dashboard responsive

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import { DashboardSidebar } from "../components/dashboard/Sidebar";
 function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <div className="flex min-h-screen bg-gray-100">
+            <div className="flex flex-col md:flex-row min-h-screen bg-gray-100">
                 <DashboardSidebar />
                 <div className="flex-1">
                     <main className="p-4">{children}</main>
